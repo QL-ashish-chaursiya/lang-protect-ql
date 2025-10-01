9d0c8fe3ba39996209d4ab145b76cc26cf5552cd
QL-ashish-chaursiya <ashishkumar.chaursiya@quokkalabs.com>
2025-10-01 14:28:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,19 @@
-import { defineConfig } from "vite";
+ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+ 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: { 
-    host: true,
-    port: 8000,
-    allowedHosts: ["site.langprotect.com"],
+  server: {
+    host: "::",
+    port: 8080,
   },
-  preview:{
-    host: true,
-    port: 8000,
-    allowedHosts:["site.langprotect.com"] 
-  },
-  build: {
-    outDir: "dist", 
-    sourcemap: mode === "production",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
